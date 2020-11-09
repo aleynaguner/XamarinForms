32c1241a6849d372ec5f8a0ebf18f3a9aaa6260d
Aleyna Güner <aleynaguner@hotmail.com>
2020-11-09 23:58:14 +0300

    MobileW1

---
+++ MobileW1/Page1.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace MobileW_01
+{
+    //Code behind
+    public partial class Page1 : ContentPage
+    {
+        public Page1()
+        {
+            InitializeComponent();
+
+            /*Label label = new Label();
+            label.Text = "Hello Friend";
+            label.FontSize = 40;
+
+            Content = label;*/
+
+           /* if (Device.RuntimePlatform==Device.iOS)
+            {
+                this.Padding = new Thickness(0, 35, 0, 0);
+            }
+
+            */
+        }
+    }
+}
